4f26c368397a4a7971a2ddbea44840dbdcd12854
Cristopher <loyoladv@gmail.com>
2025-12-22 22:32:29 -0500

    .

--- src/pages/EditorPage.tsx
+++ src/pages/EditorPage.tsx
@@ -282,8 +282,6 @@ export default function EditorPage() {
 
                         <button 
                           type="submit" 
-
-                          onClick={() => setOpen(false)}
                           className="inline-flex w-full justify-center rounded-md bg-green-700 px-3 py-2 text-sm font-semibold text-white hover:bg-green-500 sm:ml-3 sm:w-auto"
                         >Create Post</button>
                       </form>
